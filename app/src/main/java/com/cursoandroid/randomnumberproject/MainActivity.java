e9a347cb2ca683e791849e9064f37347a0ace0a2
vpdeschamp-SE <vpdeschamp@gmail.com>
2021-01-25 20:34:26 -0300

    Added random generator and button click

--- app/src/main/java/com/cursoandroid/randomnumberproject/MainActivity.java
+++ app/src/main/java/com/cursoandroid/randomnumberproject/MainActivity.java
@@ -3,6 +3,10 @@ package com.cursoandroid.randomnumberproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changeText(View view){
+
+        TextView text = findViewById(R.id.textResult);
+
+        int random = new Random().nextInt(11); //11 is not included
+
+        text.setText(""+ random);
     }
 }
